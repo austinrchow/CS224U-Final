2dc518c6286485a7a7b0ca4b2ff3ae6841c9ad76
Austin Chow <archow@stanford.edu>
2019-05-26 23:49:56 -0700

    fixing opening file

--- unigrams_without_sklearn.py
+++ unigrams_without_sklearn.py
@@ -9,24 +9,25 @@ def train_unigrams():
     for root, dirs, files in os.walk('imsdb'):
         for file in files:
             file = root + "/" + file
-
-            with open(file, 'r') as f:
-                for line in f:
-                    arr = line.split()
-                    arr.append("</s>")
-                    for word in arr:
-                        word = re.sub('\W+','', word)
-                        counts[word] = 1
-                        total_counts += 1
-
-
-    #f = open("unigrams.txt", "w")
-    print(total_counts)
-    for word, count in counts.items():
-        print(str(counts[word]) + "/" + str(total_counts))
-        prob = counts[word] / total_counts
-        print(word + " " + str(prob))
-        #f.write(word + " " + str(prob))
-    #f.close()
+            print(file)
+    #
+    #         with open(file, 'r') as f:
+    #             for line in f:
+    #                 arr = line.split()
+    #                 arr.append("</s>")
+    #                 for word in arr:
+    #                     word = re.sub('\W+','', word)
+    #                     counts[word] = 1
+    #                     total_counts += 1
+    #
+    # #
+    # #f = open("unigrams.txt", "w")
+    # print(total_counts)
+    # for word, count in counts.items():
+    #     print(str(counts[word]) + "/" + str(total_counts))
+    #     prob = counts[word] / total_counts
+    #     print(word + " " + str(prob))
+    #     #f.write(word + " " + str(prob))
+    # #f.close()
 
 train_unigrams()
